4331aecc521436f0dada6160f1818c1003a5319e
Muhammad Bagus Syahputra Tambunan <bagus.tambunan@shopee.com>
2025-12-30 16:38:21 +0700

    add 1 more dummy user

--- passkey-server/src/services/auth.service.ts
+++ passkey-server/src/services/auth.service.ts
@@ -2,10 +2,15 @@ import { User } from '../types/model';
 
 const dummyUser1: User = {
   username: 'bagus',
-  password: '123456',
+  password: '123',
 };
 
-const users: User[] = [dummyUser1];
+const dummyUser2: User = {
+  username: 'bagus2',
+  password: '123',
+};
+
+const users: User[] = [dummyUser1, dummyUser2];
 
 export const getUser = async (username: string): Promise<User | null> => {
   return users.find(user => user.username === username) || null;
