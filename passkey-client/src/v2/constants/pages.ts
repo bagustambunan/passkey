1a55e9828acca0a8ffcb6d1af569403a055d13c6
Muhammad Bagus Syahputra Tambunan <bagus.tambunan@shopee.com>
2025-12-30 17:38:36 +0700

    create login page

--- passkey-client/src/v2/constants/pages.ts
+++ passkey-client/src/v2/constants/pages.ts
@@ -1,13 +1,12 @@
 import { lazy } from "react";
-import { profile } from "../../shared/constants/profile";
 import routes from "../../shared/constants/route";
 import type { PageConfig } from "./types";
 
 export const pages: PageConfig[] = [
   {
     windowKey: "home",
-    title: profile.name,
-    icon: "🏠",
+    title: "Login",
+    icon: "🔒",
     route: routes.home,
     component: lazy(() => import("../pages/Home")),
   },
--- passkey-client/src/v2/pages/Home/index.tsx
+++ passkey-client/src/v2/pages/Home/index.tsx
@@ -1,9 +1,48 @@
-import Avatar from "../../../shared/components/Avatar";
+import { useState } from "react";
+import Button from "../../../shared/components/Button";
+import Input from "../../../shared/components/Input";
 
 export default function HomePage() {
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
   return (
     <div className="container">
-      <Avatar />
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "16px",
+          width: 300,
+        }}
+      >
+        <Input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={form.username}
+          onChange={handleChange}
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={form.password}
+          onChange={handleChange}
+        />
+        <Button onClick={handleSubmit}>Login</Button>
+      </form>
     </div>
   );
 }
